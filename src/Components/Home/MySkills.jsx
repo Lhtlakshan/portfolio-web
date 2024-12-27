1f6478aeccdd9038d844a44f087c2142f7265bb2
Thilina <thilinalakshanhewage1999@gmail.com>
2024-12-27 11:40:07 +0530

    update skills

--- src/Components/Home/MySkills.jsx
+++ src/Components/Home/MySkills.jsx
@@ -9,21 +9,20 @@ function MySkills() {
         <h2 className="text-3xl font-bold text-gray-800">My Expertise</h2>
       </div>
 
-      <div className="grid grid-cols-1 gap-8 px-4 mx-auto sm:grid-cols-2 lg:grid-cols-3 max-w-7xl">
+      <div className="w-[500px] m-4 ">
         {data?.skills?.map((skill, index) => (
           <div
             key={index}
-            className="p-6 overflow-hidden duration-300 transform bg-white rounded-lg shadow-lg hover:shadow-3xl hover:scale-110 max-w-[300px]"
+            className="w-screen p-6 m-5 overflow-hidden duration-300 transform bg-white rounded-lg shadow-l hover:shadow-3xl hover:scale-110"
           >
-            <h3 className="mb-3 text-2xl font-semibold text-blue-600">
+            <h3 className="justify-center mb-3 text-2xl font-semibold text-center text-blue-600">
               {skill.title}
             </h3>
-            {skill.tools.map((tool) => (
-              <div className="flex items-center justify-center mb-2 space-x-2">
-                <img className="w-10 h-10" src={tool.icon} />
-                <p className="font-medium text-gray-700">{tool.name}</p>
+              <div className="flex flex-row">
+                {skill.tools.map((tool) => (
+                  <img className="w-10 h-10 m-2" src={tool.icon} />
+                ))}
               </div>
-            ))}
           </div>
         ))}
       </div>
